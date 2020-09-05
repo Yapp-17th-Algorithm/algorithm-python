63fbdd79eb2bb2c255291b6d27f806733d3d814c
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-09-06 01:13:36 +0900

    Update 3nd week solution - [x] 2089 -2진수 - [x] 11576 Base Conversion - [x] 17103 골드바흐 파티션

--- 용재/3주차/17103.py
+++ 용재/3주차/17103.py
@@ -1,2 +1,26 @@
 # https://www.acmicpc.net/problem/17103
 # 골드바흐 파티션
+
+import math
+
+count = 1000001
+primes = [False, False] + [True] * (count - 1)
+
+for i in range(2, count + 1):
+  if primes[i]:
+    for j in range(2 * i, count + 1, i):
+        primes[j] = False
+
+T = int(input())
+
+N = [int(input()) for _ in range(T)]
+
+for each in N:
+  answer = 0
+  for i in range(2, math.floor(each / 2) + 1):
+    if primes[i] == True and primes[each - i] == True:
+      answer += 1
+    else:
+      continue
+
+  print(answer)

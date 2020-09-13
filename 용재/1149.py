c486c81123a76317ae30c9e57d1699cae6149f38
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-09-13 17:34:39 +0900

    Update 4th week solution to reflect code review

--- 용재/1149.py
+++ 용재/1149.py
@@ -7,12 +7,9 @@ inputColors = [list(map(int, input().split())) for _ in range(N)]
 
 colorSum = [[0] * 3 for _ in range(N)]
 
-i = 0
-
-for each in inputColors:
+for i, each in enumerate(inputColors):
   colorSum[i][0] = each[0] + min(colorSum[i - 1][1], colorSum[i - 1][2])
   colorSum[i][1] = each[1] + min(colorSum[i - 1][0], colorSum[i - 1][2])
   colorSum[i][2] = each[2] + min(colorSum[i - 1][0], colorSum[i - 1][1])
-  i += 1
 
-print(min(colorSum[N - 1][0], colorSum[N - 1][1], colorSum[N-1][2]))
+print(min(colorSum[N - 1]))

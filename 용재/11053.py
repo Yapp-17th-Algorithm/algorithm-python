98abceb5e2ddd5adc04b1fa12707fd0ddd6fb2ed
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-09-20 17:23:34 +0900

    Update 5th week solution to reflect code review

--- 용재/11053.py
+++ 용재/11053.py
@@ -10,6 +10,6 @@ dp = [1] * N
 for i in range(N):
   for j in range(i):
     if A[j] < A[i]:
-      dp[i] = max(dp[i], dp[j]+1)
+      dp[i] = max(dp[i], dp[j] + 1)
 
 print(max(dp))
--- 용재/2156.py
+++ 용재/2156.py
@@ -14,9 +14,11 @@ if n == 2:
 if n > 2:
   dp[0] = glasses[0]
   dp[1] = dp[0] + glasses[1]
-  dp[2]=max(dp[1], dp[0] + glasses[2], glasses[1] + glasses[2])
+  dp[2] = max(dp[1], dp[0] + glasses[2], glasses[1] + glasses[2])
 
   for i in range(3, n):
-    dp[i] = max(dp[i-1], dp[i-2] + glasses[i], dp[i-3] + glasses[i] + glasses[i-1])
+    dp[i] = max(dp[i - 1],
+                dp[i - 2] + glasses[i],
+                dp[i-3] + glasses[i] + glasses[i - 1])
 
-  print(dp[n-1])
+  print(dp[n - 1])
